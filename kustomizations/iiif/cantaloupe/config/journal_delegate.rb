17c438e2d3ca8c87892ed338499e8a822feeac6e
A. K <74007089+soggy-mushroom@users.noreply.github.com>
2025-07-14 14:56:13 +0100

    Make cantaloupe prefixes configurable
    https://github.com/elifesciences/issues/issues/9340

--- kustomizations/iiif/cantaloupe/config/journal_delegate.rb
+++ kustomizations/iiif/cantaloupe/config/journal_delegate.rb
@@ -27,16 +27,16 @@ class CustomDelegate
     logger.info 'identifier: ' << identifier
 
     if identifier.start_with?('lax:') or identifier.start_with?('lax/') then
-      logger.info 'prefix: ' << identifier[0..2] << ' is HTTPSource ' << 'https://s3-external-1.amazonaws.com/prod-elife-published/articles/'
-      return 'https://s3-external-1.amazonaws.com/prod-elife-published/articles/' <<  identifier[4..-1]
+      logger.info 'prefix: ' << identifier[0..2] << ' is HTTPSource ' << '${lax_prefix_url}'
+      return '${lax_prefix_url}' <<  identifier[4..-1]
     end
     if identifier.start_with?('digests:') or identifier.start_with?('digests/') then
-      logger.info 'prefix: ' << identifier[0..6] << ' is HTTPSource ' << 'https://s3-external-1.amazonaws.com/prod-elife-published/digests/'
-      return 'https://s3-external-1.amazonaws.com/prod-elife-published/digests/' <<  identifier[8..-1]
+      logger.info 'prefix: ' << identifier[0..6] << ' is HTTPSource ' << '${digests_prefix_url}'
+      return '${digests_prefix_url}' <<  identifier[8..-1]
     end
     if identifier.start_with?('journal-cms:') or identifier.start_with?('journal-cms/') then
-      logger.info 'prefix: ' << identifier[0..10] << ' is HTTPSource ' << 'https://prod--journal-cms.elifesciences.org/sites/default/files/iiif/'
-      return 'https://prod--journal-cms.elifesciences.org/sites/default/files/iiif/' <<  identifier[12..-1]
+      logger.info 'prefix: ' << identifier[0..10] << ' is HTTPSource ' << '${journal_cms_prefix_url}'
+      return '${journal_cms_prefix_url}' <<  identifier[12..-1]
     end
     nil
   end
